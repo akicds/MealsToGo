e8fb2a4aab5f0fce3d6ffcdba28aca9083ad6307
akicds <halilberk.aki.dev@gmail.com>
2024-12-10 16:34:13 +0300

    feature additions such as map pins, favourites etc

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,16 +1,73 @@
-import React, { useContext, useState, useEffect } from 'react';
-import MapView from 'react-native-maps';
-import styled from 'styled-components/native';
-import { Search } from '../components/search.component';
- 
+import React, { useContext, useState, useEffect } from "react";
+import MapView, { Marker, Callout } from "react-native-maps";
+import styled from "styled-components/native";
+
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { RestaurantsNavigator } from "../../../infrastructure/navigation/restaurants.navigator";
+
+import { Search } from "../components/search.component";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import { MapCallout } from "../components/map-callout.component";
+
 const Map = styled(MapView)`
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 `;
 
-export const MapScreen = () => (
+export const MapScreen = ({ navigation }) => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location|| {};
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
     <>
-        <Search/>
-        <Map />
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02
+        }}
+      >
+        {restaurants.map(restaurant => {
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng
+              }}
+            >
+              <Callout
+                onPress={() =>
+                  navigation.navigate("Restaurants", {
+                    screen: "RestaurantDetail",
+                    params: { restaurant },
+                  })
+                }
+              >
+                <View>
+                  <MapCallout restaurant={restaurant} />
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
+      </Map>
     </>
-);
+  );
+};
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,9 +3,11 @@ import React from "react";
 // import { Card } from "react-native-paper";
 // import styled from "styled-components/native";
 import { SvgXml } from 'react-native-svg';
+import { View } from "react-native";
 
+import { Favourite } from "../../../components/favourites/favourite.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-import { Text } from "../../../components/typhography/text.component";
+import { Text } from "../../../components/typography/text.component";
 import star from '../../../../assets/star';
 import open from '../../../../assets/open';
 // import { sizes } from "../../../infrastructure/theme/sizes";
@@ -41,7 +43,10 @@ console.log(ratingArray);
 
   return (
     <RestaurantCard elevation={5}>
-      <RestaurantCardCover key={name} source={photos ? { uri: photos[0] } : require('../../../../assets/foodphoto.png')} />
+      <View>
+        <Favourite restaurant={restaurant}/>
+        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      </View>
       <Info>
         <Text variant= "label">{name}</Text>
         <Section>
